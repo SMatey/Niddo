9665386b9d69e83561c3b441c19be0d0512584cf
Sebastian Matey <casamarosebas@gmail.com>
2026-05-23 15:36:50 -0600

    Update page.tsx

--- src/app/(dashboard)/mis-publicaciones/[id]/editar/page.tsx
+++ src/app/(dashboard)/mis-publicaciones/[id]/editar/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useEffect, useState } from 'react'
 import { PropertyPublicationForm } from '@/features/properties/components/property-publication-form'
 import { useParams } from 'next/navigation'
