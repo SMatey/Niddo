fb146113dadd627fb2437cd0c40bd0a54a6583b5
LuisMendezTEC <lmendez@estudiantec.cr>
2026-06-07 20:52:25 -0600

    Refactor: new profile-review-card.utils and moving some interfaces to types directory

--- src/features/reviews/profile-reviews/index.ts
+++ src/features/reviews/profile-reviews/index.ts
@@ -1,3 +1,4 @@
 export { ProfileReviewsSection } from './components/profile-reviews-section'
 export { useProfileReviews } from './hooks/use-profile-reviews'
 export { loadProfileReviews } from './services/profile-reviews.service'
+export type { UseProfileReviewsResult } from '@/features/reviews/types/review-hook.types'
